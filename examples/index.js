33127008c9449ea1a7c9f06417a468e3a5732bbd
Jona Andersen <jona@ndrsn.org>
2022-02-23 16:35:59 -0600

    Update index.js

--- examples/index.js
+++ examples/index.js
@@ -1,1 +1,11 @@
-// Nothing here
+import canvasDatagrid from 'canvas-datagrid';
+
+const gridElement = document.getElementById('grid');
+
+canvasDatagrid({
+  parentNode: gridElement,
+  data: [
+    { foo: 1, bar: 2, baz: 3 },
+    { foo: 4, bar: 5, baz: 6 },
+  ],
+});
